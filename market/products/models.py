4c5d71db561542e141671e58179db53c3a8663b1
Valerii Nataleev <valetnat91@mail.ru>
2023-09-03 14:25:08 +0300

    add_category_model

--- market/products/models.py
+++ market/products/models.py
@@ -21,3 +21,21 @@ class ProductDetail(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     detail = models.ForeignKey(Detail, on_delete=models.CASCADE)
     value = models.CharField(max_length=128, verbose_name=_("значение"))
+
+
+class Category(models.Model):
+    """Категория продукта"""
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+    name = models.CharField(max_length=128, unique=True, verbose_name=_("наименование"))
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name=_("дата создания"))
+    modified_at = models.DateTimeField(auto_now=True, verbose_name=_("дата последнего изменения"))
+    archived = models.BooleanField(default=False, verbose_name=_("архивировано"))
+
+    parent = models.ForeignKey("self", null=True, verbose_name=_("родитель"), on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f"Категория(pk={self.pk}, name={self.name!r})"
