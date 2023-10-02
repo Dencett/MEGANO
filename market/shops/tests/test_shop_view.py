30784d25813453d3056d413a08d735113b60afa6
Дмитрий Востриков <dimanusnn@mail.ru>
2023-10-02 20:31:17 +0300

    Update market/fixtures/02-groups.json, market/profiles/tests/tests.py, market/shops/tests/test_shop_view.py

--- market/shops/tests/test_shop_view.py
+++ market/shops/tests/test_shop_view.py
@@ -18,9 +18,9 @@ User = get_user_model()
 
 
 FIXTURES = [
-    "fixtures/0-1-groups.json",
-    "fixtures/0-01-users.json",
-    "fixtures/0-04-shops.json",
+    "fixtures/01-users.json",
+    "fixtures/02-groups.json",
+    "fixtures/04-shops.json",
     "fixtures/05-category.json",
     "fixtures/06-tags.json",
     "fixtures/07-products.json",
@@ -158,68 +158,3 @@ class ShopModelTestCase(TestCase):
         self.assertEqual(self.shop.avatar, "media/shops/1/avatar/dns.jpg")
         self.assertEqual(self.shop.user.username, "test_user_for_shop_test")
         self.assertEqual(self.shop.user.email, "test_user@mail.com")
-
-
-# name = "Test shop 55",
-# about = "Test description test shop 123124312",
-# phone = "89006663322",
-# email = "test-shop-reatailer@mail.com",
-# avatar = "media/shops/1/avatar/dns.jpg",
-# # ===========================================
-# class ProductModelTest(TestCase):
-#     """Класс тестов модели Продукт"""
-#
-#     @classmethod
-#     def setUpClass(cls):
-#         cls.category = Category.objects.create(name="Тестовая категория")
-#
-#         cls.detail = Detail.objects.create(name="тестовая характеристика")
-#         cls.product = Product.objects.create(
-#             name="Тестовый продукт",
-#             category=cls.category,
-#         )
-#         cls.product.details.set([cls.detail], through_defaults={"value": "тестовое значение"})
-#
-#     @classmethod
-#     def tearDownClass(cls):
-#         cls.detail.delete()
-#         cls.product.delete()
-#         cls.category.delete()
-#
-#     def test_verbose_name(self):
-#         product = ProductModelTest.product
-#         field_verboses = {
-#             "name": "наименование",
-#             "details": "характеристики",
-#         }
-#         for field, expected_value in field_verboses.items():
-#             with self.subTest(field=field):
-#                 self.assertEqual(product._meta.get_field(field).verbose_name, expected_value)
-#
-#     def test_name_max_length(self):
-#         product = ProductModelTest.product
-#         max_length = product._meta.get_field("name").max_length
-#         self.assertEqual(max_length, 512)
-#
-#     def test_productdetail_value(self):
-#         productdetail = ProductDetail.objects.all()[0]
-#         value = productdetail.value
-#         self.assertEqual(value, "тестовое значение")
-#
-#     def test_product_fields_name(self):
-#         attr_names = dir(Product())
-#         variable_name_in_template = (
-#             "shops",
-#             "preview",
-#             "productimage_set",
-#             "name",
-#             "about",
-#             "tags",
-#             "description",
-#             "offer_set",
-#             "productdetail_set",
-#         )
-#         for name in variable_name_in_template:
-#             self.assertIn(name, attr_names)
-
-# ===========================================
