6d851e32fc2a526d59412e2abcd8eaf0b5838a47
Elmir Zinattullin <dd2011ld@yandex.ru>
2023-10-08 18:40:18 +0300

    test migrations <dd2011ld@yandex.ru>
    Signed-off-by: Elmir Zinattullin <dd2011ld@yandex.ru>

--- market/catalog/models.py
+++ market/catalog/models.py
@@ -1,3 +1,12 @@
+from django.db import models
+
+
+class CatalogTestmodel(models.Model):
+    offer = models.ForeignKey(to="shops.Offer", on_delete=models.CASCADE)
+    user = models.ForeignKey(to="profiles.User", on_delete=models.CASCADE, related_name="test")
+    amount = models.IntegerField(default=1)
+
+
 # from django.db import models
 
 # Create your models here.
