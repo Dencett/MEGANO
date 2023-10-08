715b9fdc32215179c507a00e61aebf35f79446fe
Эльмир Зинаттуллин <dd2011ld@yandex.ru>
2023-10-08 18:09:28 +0300

    Revert "gitlab test"
    This reverts commit 4ed7e3398d0109b35e6e831b9ac206532c3692a2

--- market/cart/apps.py
+++ market/cart/apps.py
@@ -1,11 +1,10 @@
 from django.apps import AppConfig
-
-# from django.core import management
+from django.core import management
 
 
 class CartConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "cart"
-    #
-    # def ready(self):
-    #     management.call_command("clearsessions")
+
+    def ready(self):
+        management.call_command("clearsessions")
