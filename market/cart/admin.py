8a21c64bb3e5531e28fb7d3d9c7d653e81576693
Elmir Zinattullin <dd2011ld@yandex.ru>
2023-10-16 22:21:57 +0300

    soft-delete for UserOfferCart
    Signed-off-by: Elmir Zinattullin <dd2011ld@yandex.ru>

--- market/cart/admin.py
+++ market/cart/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin  # noqa
+from .models import UserOfferCart
 
 # Register your models here.
+
+
+@admin.register(UserOfferCart)
+class UserOfferCartAdmin(admin.ModelAdmin):
+    list_display = [
+        "offer",
+        "user",
+        "amount",
+    ]
--- market/cart/models.py
+++ market/cart/models.py
@@ -3,9 +3,16 @@ from django.utils.translation import gettext_lazy as _
 
 
 class UserOfferCart(models.Model):
+    """Модель для упаковки товара с определенным количеством
+    Если поле is_active = True то модель используется для текущей корзины пользователя.
+    Совокупность моделей с is_active = True, будет являться текущей пользовательской корзиной.
+    Если поле is_active = False, то модель будет использоваться для отображения модели уже оплаченного ЗАКАЗА.
+    """
+
     offer = models.ForeignKey(to="shops.Offer", on_delete=models.CASCADE)
     user = models.ForeignKey(to="profiles.User", on_delete=models.CASCADE, related_name="cart")
     amount = models.IntegerField(default=1)
+    is_active = models.BooleanField(default=True)
 
     class Meta:
         verbose_name = _("запись корзины")
