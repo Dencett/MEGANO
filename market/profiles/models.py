eb789e36919a5751541bb5a09608953cde4f3fc0
dencett <vova.denezhkin.98@mail.ru>
2023-11-20 20:26:15 +0300

    Added validation for the phone number and removed the ban on migration.

--- market/profiles/models.py
+++ market/profiles/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, PermissionsMixin
 from django.utils.translation import gettext_lazy as _
+from django.core.validators import RegexValidator
 
 
 def profile_directory_path(instance: "User", filename):
@@ -12,7 +13,13 @@ def profile_directory_path(instance: "User", filename):
 
 class User(AbstractUser, PermissionsMixin):
     email = models.EmailField(_("email address"), unique=True)
-    phone = models.CharField(_("Номер телефона"), max_length=11, default="")
+    phone = models.CharField(
+        _("Номер телефона"),
+        default="",
+        validators=[
+            RegexValidator(regex=r"\+[7]\([0-9]{3}\)[0-9]{3}\-[0-9]{2}\-[0-9]{2}", message=_("номер некорректный"))
+        ],
+    )
     residence = models.CharField(
         _("Город проживания"),
         max_length=100,
