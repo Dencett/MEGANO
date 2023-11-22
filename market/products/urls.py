5a72b0e88ab3775871c68f7a635241e2f26fc9fa
Elmir Zinattullin <dd2011ld@yandex.ru>
2023-11-22 21:13:36 +0300

    fix product-detail page: Отображение аватаров во вкладке Отзывы
    Signed-off-by: Elmir Zinattullin <dd2011ld@yandex.ru>

--- market/products/urls.py
+++ market/products/urls.py
@@ -7,6 +7,4 @@ app_name = "products"
 urlpatterns = [
     path("", HomeView.as_view(), name="home-page"),
     path("product/<int:pk>/", ProductView.as_view(), name="product-detail"),
-    # path("temp/", MyTemplateView.as_view(), name='temp'),
-    # path('test/', myget, name='test')
 ]
